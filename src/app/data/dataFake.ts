239ca7dcd4680e2a10a171319e87adb5705774ca
Vinicius Gomes <viniciusgomes.web@gmail.com>
2023-04-07 19:30:34 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "saiu novo filme",
+    "description": "Marvel anuncia lançamento de novo filme do Homem de Ferro, confira",
+    "photoCover": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2023/03/iron-man-comics-marvel-2973278.jpg?itok=wOPnWcYx"
+  },
+
+  {
+    "id": "2",
+    "title": "Nova Série anunciada no Disney+",
+    "description": "blabla blalbla",
+    "photoCover": "https://s.aficionados.com.br/imagens/rumor-vingadores-4-acontecera-cinco-anos-no-futuro_f.jpg"
+  },
+
+  {
+    "id": "3",
+    "title": "Nova série sobre a familia Stark será lançada em breve",
+    "description": "blabla blalbla",
+    "photoCover": "https://marvelucm.com/wp-content/uploads/2021/12/tonii.jpg"
+  },
+
+  {
+    "id": "4",
+    "title": "Marvel anuncia lançamento do novo filme Pantera Negra",
+    "description": "blabla blalbla",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  }
+]
+
